0e02b54d1227d15a0250555dda2435e65d3ed83f
Usuario <Usuario@CPD03-M13>
2024-03-04 15:02:18 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiAppJogoVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "0"; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_clicked (object sender, EventArgs e)
+        {
+            Button disparador = (button)sender;
+            disparador.IsEnabled = false;
+            if(vez=="0"
+            {
+                disparador.Text = "0";
+                vez = "x";
+
+            }else
+            { }
+    }
+
+}
