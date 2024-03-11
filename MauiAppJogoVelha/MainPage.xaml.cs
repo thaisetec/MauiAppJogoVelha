dbbb203d7835b404babda8fd0cc1360505dd2ce1
Usuario <Usuario@CPD03-M13>
2024-03-11 15:09:17 -0300

    jogo

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -2,24 +2,78 @@
 {
     public partial class MainPage : ContentPage
     {
-        string vez = "O"; 
+        String vez = "O";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void button_clicked (object sender, EventArgs e)
+        private void button_clicked(object sender, EventArgs e)
         {
-            Button disparador = (button)sender;
+            Button disparador = (Button)sender;
+
             disparador.IsEnabled = false;
-            if(vez=="0"
+
+            if (vez == "O")
             {
-                disparador.Text = "0";
-                vez = "x";
+                disparador.Text = "O";
+                vez = "X";
+            }
+            else
+            {
+                disparador.Text = "X";
+                vez = "O";
+            }
+            if (btn10.Text == "X"
+                && btn11.Text == "X"
+                && btn12.Text == "X")
+            {
+                DisplayAlert("Parabéns", "o X ganhou!", "OK");
 
-            }else
-            { }
+            }
+            if (btn10.Text == "O"
+                && btn11.Text == "O"
+                && btn12.Text == "O")
+            {
+                DisplayAlert("Parabéns", "o O ganhou!", "OK");
+            }
+            if (btn20.Text == "X"
+                && btn21.Text == "X"
+                && btn22.Text == "X")
+            {
+                DisplayAlert("Parabéns", "o X ganhou!", "OK");
+            }
+            if (btn20.Text == "O"
+               && btn21.Text == "O"
+               && btn22.Text == "O")
+            {
+                DisplayAlert("Parabéns", "o O ganhou!", "OK");
+            }
+            if (btn30.Text == "X"
+                && btn31.Text == "X"
+                && btn32.Text == "X")
+            {
+                DisplayAlert("Parabéns", "o X ganhou!", "OK");
+            }
+            if (btn30.Text == "O"
+              && btn31.Text == "O"
+              && btn32.Text == "O")
+            {
+                DisplayAlert("Parabéns", "o O ganhou!", "OK");
+            }
+            if (btn12.Text == "X"
+            && btn21.Text == "X"
+            && btn30.Text == "X")
+            {
+                DisplayAlert("Parabéns", "o X ganhou!", "OK");
+            }
+            if (btn12.Text == "O"
+              && btn21.Text == "O"
+              && btn30.Text == "O")
+            {
+                DisplayAlert("Parabéns", "o O ganhou!", "OK");
+            }
+        }
     }
-
 }
